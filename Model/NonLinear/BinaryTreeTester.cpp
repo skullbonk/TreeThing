87eb8cff29842a7a36bbfc11fc8972b7a41bc85c
Skullbonk <reagan.fairbanks@gmail.com>
2019-04-16 13:14:03 -0600

    added some stuff in probably the wrong spots

---
+++ Model/NonLinear/BinaryTreeTester.cpp
@@ -0,0 +1,15 @@
+//
+//  BinaryTreeTester.cpp
+//  TreeThing
+//
+//  Created by Fairbanks, Reagan on 4/16/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <stdio.h>
+#include "BinaryTreeTester.hpp"
+
+void BinaryTreeTester :: treeStuff()
+{
+	
+}
---
+++ Model/NonLinear/BinaryTreeTester.hpp
@@ -0,0 +1,25 @@
+//
+//  BinaryTreeTester.hpp
+//  TreeThing
+//
+//  Created by Fairbanks, Reagan on 4/16/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include "../Model/Nodes/BinaryTreeNode.hpp"
+#include "../Model.NonLinear/BinarySearchTree.hpp"
+
+class BinaryTreeTester
+{
+private:
+	BinaryTreeNode<int> testNode;
+	BinarySearchTree<int> testTree;
+public:
+	void treeStuff();
+};
+
+
+#endif /* BinaryTreeTester_hpp */
